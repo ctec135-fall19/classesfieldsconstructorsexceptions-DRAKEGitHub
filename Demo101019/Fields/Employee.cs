a8cabb33f20c733e6f4763fde6f29ef1ddfa829a
Allison Drake <A.DRAKE4@students.clark.edu>
2019-10-10 15:19:31 -0700

    Complete Thursday Lab. May be under allisonnicoledrake@gmail.com or a.drake4@students.clark.edu

---
+++ Demo101019/Fields/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    class Employee
+    {
+        // fields
+        private string empName;
+        private int empID;
+        private double currPay;
+
+        // accessor and mutator methods
+        public string GetName() {return empName; }
+        public int GetEmpID() { return empID; }
+        public double GetCurryId() { return currPay; }
+
+        public void SetName(string name)
+        {
+            // perform validation
+            if (name.Length > 15) { empName = name.Substring(0, 15); }
+            else empName = name;
+        }
+        public void SetEmpID(int id) { empID = id; }
+        public void SetCurrPay(double pay) { currPay = pay; }
+
+        //methods
+        public void PrintState()
+        {
+            Console.WriteLine("Name:\t{0}", empName);
+            Console.WriteLine("ID:\t{0}", empID);
+            Console.WriteLine("Pay:\t{0}", currPay);
+        }
+
+    }
+}
---
+++ Demo101019/Problem3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region region 1
+            for(int printerPrintsCondition = 0; printerPrintsCondition <= 1; printerPrintsCondition++)
+            {
+                // cond2 = red light is flashing
+                for(int cond2 = 0; cond2 <= 1; cond2++)
+                {
+                    for(int bananasCondition = 0; bananasCondition <= 1; bananasCondition++)
+                    {
+                        if(printerPrintsCondition == 0 && cond2 == 0 && bananasCondition == 0) 
+                        {
+                            // call action methods
+                            Console.WriteLine("Print prints condition: NO");
+                            // call action methods
+                        }
+                        if(printerPrintsCondition == 0 && cond2 == 0 && bananasCondition == 1)
+                        {
+                            // call action methods
+                            // printer jam
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            #region region 2
+            for (int i =0; i <= 7; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        // print out conditions
+                        // call action methods for NO, NO, NO
+                        break;
+                    case 1:
+                        //print our conditions
+                        // call action methods for No, No, Yes
+                        break;
+                }
+            }
+            #endregion
+        }
+
+        // Create the Methods
+
+        public void CheckInk()
+        {
+            Console.WriteLine("Check ink and replace if empty.");
+        }
+        public void PowerCable()
+        {
+            Console.WriteLine("Check the power cable connection.");
+        }
+    }
+}
